786d2f265f01178dcfa59377a8a41b9f8929103e
pietrodub <pietroti453@gmail.com>
2026-03-22 12:56:30 -0300

    adição do construct no authController

--- DampCode_API/Controllers/AuthController.cs
+++ DampCode_API/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DampCode_API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebApiMongoDbDemo.Data;
 
 namespace DampCode_API.Controllers
 {
@@ -7,5 +10,13 @@ namespace DampCode_API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IMongoCollection<User> _users;
+        public AuthController(MongoDbService mongoDbService)
+        {
+            _users = mongoDbService.Database?.GetCollection<User>("user");
+        }
+
+        //CREATE
+        //[HttpPost]
     }
 }
