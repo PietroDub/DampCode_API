3c92f3e19fc5c00a28505c33d910a95e721116ac
pietrodub <pietroti453@gmail.com>
2026-03-21 23:55:15 -0300

    Ajustando DTO's e criando controlers de user.

---
+++ DampCode_API/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DampCode_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
--- DampCode_API/Dto/EmpresaDto.cs
+++ DampCode_API/Dto/EmpresaDto.cs
@@ -5,28 +5,14 @@ namespace DampCode_API.Dto
 {
     public class EmpresaDto
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.String)]
         public required string Id { get; set; }
-        [BsonElement("name"), BsonRepresentation(BsonType.String)]
         public required string Name { get; set; }
-        [BsonElement("email"), BsonRepresentation(BsonType.String)]
         public required string Email { get; set; }
-        [BsonElement("password"), BsonRepresentation(BsonType.String)]
         public required string Password { get; set; }
 
-        //DEFINIÇÃO DO TIPO
-        [BsonElement("role"), BsonRepresentation(BsonType.String)]
-        public required string Role { get; set; }
-
         //Empresa
-        [BsonElement("cnpj"), BsonRepresentation(BsonType.String)]
         public string? Cnpj { get; set; }
-
-        [BsonElement("verificado"), BsonRepresentation(BsonType.Boolean)]
         public bool? Verificado { get; set; }
-
-        [BsonElement("hackatonsIds")]
         public List<string>? HackatonsIds { get; set; }
     }
 }
