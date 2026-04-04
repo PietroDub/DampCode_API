5da791440d572cc229528cf0d1406ad132c14c28
pietrodub <pietroti453@gmail.com>
2026-04-04 07:47:02 -0300

    Adição de DTO'S futuros.

---
+++ DampCode_API/Dto/CreateHackatonDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DampCode_API.Dto
+{
+    public class CreateHackathonDTO
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public List<string>? Technologies { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ DampCode_API/Dto/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DampCode_API.Dto
+{
+	public class LoginDTO
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
